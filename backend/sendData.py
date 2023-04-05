d5cf750f373fd12ff0ddc371e6b0d4761eede1be
Adi Patel <toadipatel07@gmail.com>
2023-04-05 12:50:11 -0400

    test

---
+++ backend/sendData.py
@@ -0,0 +1,10 @@
+# <sendData.py>
+# 1) send numerical data to firebase
+# 2) please check notion task called ".env configuration" for environmental variable (firebase configuration)
+
+# feel free to change format to test by yourself
+
+from dotenv import load_dotenv
+
+def run(data):
+    load_dotenv()
