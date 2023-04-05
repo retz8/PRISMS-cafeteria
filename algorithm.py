0c9a5b66514afe2d88b4237871907ba36185762e
ss77ii <ziruiwang.spencer@gmail.com>
2023-04-05 12:59:29 -0400

    :)

---
+++ algorithm.py
@@ -0,0 +1,23 @@
+# <algorithm.py> additional algorithm for AI model
+# 1) receive detection result using base model & frame
+# 2) add additional algorithm to improve accuracy and acheive our goal of counting # of ppl eating each meal
+# 3) return updated number of ppl 
+
+# for testing, feel free to change to funciton function like
+# def process(self):
+# then, define results or img by your own mock data
+# or you can change the format by yourself to test
+
+
+import cv2
+import torch
+
+def process(self, results, img):
+
+    number = 0
+
+
+
+    # return updated number of ppl eating (or smt like +1, -2)
+    return number
+
