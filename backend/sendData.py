c4c6aed999ddac85e3f798409e28c2db3eac940c
Adi Patel <toadipatel07@gmail.com>
2023-04-07 13:08:40 -0400

    update

--- backend/sendData.py
+++ backend/sendData.py
@@ -1,9 +1,26 @@
-# <sendData.py>
-# 1) send numerical data to firebase
-# 2) please check notion task called ".env configuration" for environmental variable (firebase configuration)
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+import json
 
-# feel free to change format to test by yourself
+def run(data):
+    load_dotenv()
+    URL = os.getenv("FIREBASE_REALTIME_DATABASE_URL") 
+    with open("keys.json") as f:
+        SECRET = json.load(f)
 
-def run(self, data):
-    print("Hello World")
-    # send data
+    ### Firebase Connection
+    cred = credentials.Certificate(SECRET)
+    firebase_admin.initialize_app(cred, {
+        "databaseURL": URL
+    })
+
+    ref = db.reference("/")
+    data = list(data)
+    ref.set({
+        "data": data
+    })
+    print("sent data")
