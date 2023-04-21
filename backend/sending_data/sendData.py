e8c1f83bf5e0653b537c65ce475f39154c99217e
Adi Patel <toadipatel07@gmail.com>
2023-04-21 12:57:03 -0400

    Updated instructions and added error handling

--- backend/sending_data/sendData.py
+++ backend/sending_data/sendData.py
@@ -3,23 +3,28 @@ from firebase_admin import credentials
 from firebase_admin import db
 from dotenv import load_dotenv
 import os
+from typing import List
 import json
 
-def run(data):
+def run(data) -> tuple[List[int], str]:
     load_dotenv()
     URL = os.getenv("FIREBASE_REALTIME_DATABASE_URL") 
     with open("keys.json") as f:
         SECRET = json.load(f)
 
     ### Firebase Connection
-    cred = credentials.Certificate(SECRET)
-    firebase_admin.initialize_app(cred, {
-        "databaseURL": URL
-    })
+    try:
+        cred = credentials.Certificate(SECRET)
+        firebase_admin.initialize_app(cred, {
+            "databaseURL": URL
+        })
+        assert cred != None, "Firebase credentials are not initialized."
 
-    ref = db.reference("/")
-    data = list(data)
-    ref.set({
-        "data": data
-    })
-    print("Sent data to firebase!")
+        ref = db.reference("/")
+        data = list(data)
+        ref.set({
+            "data": data
+        })
+        return (data, "SENT DATA TO FIREBASE")
+    except:
+        return (data, "ERROR SENDING DATA TO FIREBASE")
