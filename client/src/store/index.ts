fe010c01e3dc018b52cb1ab68e6a76c032c1eea8
retz8 <ekkicb71@gmail.com>
2023-06-04 21:46:17 -0400

    ready to deploy

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,5 +1,5 @@
 import { proxy } from "valtio";
-import { Data, MockData } from "../model/data";
+import { MockData } from "../model/data";
 
 // like useContext
 // change Data to MockData in publication
